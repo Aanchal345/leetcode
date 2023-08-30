fa11722b4fd75eab4e4c28d6c18f001cd137e2eb
Aanchal345 <135842685+Aanchal345@users.noreply.github.com>
2023-08-30 20:45:09 +0530

    Time: 33 ms (40.48%), Space: 13.4 MB (61.04%) - LeetHub

---
+++ 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isAcronym(self, words, s):
+        """
+        :type words: List[str]
+        :type s: str
+        :rtype: bool
+        """
+        return ''.join(w[0]for w in words)==s
+        
+      
+        
