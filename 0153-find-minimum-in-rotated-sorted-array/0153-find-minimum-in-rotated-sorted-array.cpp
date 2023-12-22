2be33a0a31c1b528966020ab656dbaf27841e32d
Aanchal345 <135842685+Aanchal345@users.noreply.github.com>
2023-12-22 16:14:10 +0530

    Time: 0 ms (100.00%), Space: 10.4 MB (67.46%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -5,8 +5,8 @@ public:
     int e = nums.size()-1;
 	while(s < e){
 		int m= s+ (e - s)/2;
-		if(nums[m]>nums[e]) s = m+1;
-		else e = m;
+		if(nums[m]>nums[e]){ s = m+1;}
+		else {e = m;}
 	}
 	return nums[s];
         
