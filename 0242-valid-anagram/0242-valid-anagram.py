ff11ec8761502aa7772d81fd4f7defc5e4bc53c8
Aanchal345 <135842685+Aanchal345@users.noreply.github.com>
2024-01-16 18:35:22 +0530

    Time: 11 ms (99.86%), Space: 13.7 MB (90.47%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        for x in 'abcdefghijklmnopqrstuvwxyz':
+            if s.count(x) != t.count(x):
+                return False
+        return True
